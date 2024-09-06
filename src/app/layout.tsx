da27767f0b8a52135e578a786b9f3bfe435f5b32
ShadyTiger <chatterjeesoumyajeet@gmail.com>
2024-09-06 17:14:55 +0530

    CHANGE: project metadata changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,54 @@ import ToasterProvider from "./providers/toaster-provider";
 import { Toaster } from "~/components/ui/sonner";
 
 export const metadata: Metadata = {
-  title: "Planit - Advanced Task Management",
+  title: "Planit | Advanced Task Management & Productivity App",
   description:
-    "Planit is an advanced to-do application meticulously crafted to revolutionize task management and enhance productivity. Built with the modern tech stack of Next.js, Tailwind CSS, and MongoDB, Planit offers a seamless and efficient way to organize your tasks, whether you're working solo or collaborating with a team.",
+    "Boost productivity and streamline task management with Planit, the cutting-edge to-do app designed for individuals and teams. Powered by Next.js, Tailwind CSS, and MongoDB, Planit offers advanced task organization, project management, and collaboration features. Achieve more with intuitive UI and seamless integration.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  keywords: [
+    "Planit",
+    "task management app",
+    "to-do app",
+    "productivity tool",
+    "project management",
+    "team collaboration",
+    "Next.js app",
+    "Tailwind CSS",
+    "MongoDB",
+    "task organization",
+    "task tracking",
+    "advanced task manager",
+    "Kanban",
+    "Kanban Board",
+    "Kanban Todo"
+  ],
+  openGraph: {
+    title: "Planit | Advanced Task Management & Productivity App",
+    description:
+      "Discover Planit, a powerful task management and productivity app that simplifies your workflow and enhances team collaboration. Built with modern technologies for maximum efficiency.",
+    url: "https://planit-pi.vercel.app/", // Update this with the actual site URL
+    siteName: "Planit",
+    images: [
+      {
+        url: "/hero.png", // Update this with an actual image path
+        width: 1200,
+        height: 630,
+        alt: "Planit - Advanced Task Management App",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   site: "@yourTwitterHandle", // Update with your Twitter handle
+  //   title: "Planit | Advanced Task Management & Productivity App",
+  //   description:
+  //     "Maximize productivity and streamline task management with Planit. Built for teams and individuals to effectively organize and complete tasks.",
+  //   image: "/images/twitter-image.png", // Update with an actual image path
+  // },
 };
+
 
 
 const ubuntu = Ubuntu({
